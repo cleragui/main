44b4ff635491c26359904c65fb3ba20b82283e78
clera <130056677+cleragui@users.noreply.github.com>
2025-01-24 20:20:03 +0000

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Clera'
-copyright = '2023, GNU LGPLv3'
+copyright = '2025, GNU LGPLv3'
 author = 'Erasmus A. Junior'
 # release = '0.2.4'
 
